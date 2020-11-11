331b29ea6c82ffd2b970204623b14ff93212916a
landerton1 <74026062+landerton1@users.noreply.github.com>
2020-11-11 13:47:21 -0600

    Add files via upload

---
+++ not a rick roll.bat
@@ -0,0 +1,42 @@
+@echooff
+color 5
+title dont press enter!!!
+cls
+
+@echo off
+:loop
+set /a num=5
+color %num%
+                                                                            
+echo                            ````````````````                                                        
+echo                        .:ohmddddddddddddddddy/.                                                    
+echo                     .+ddmmddddddmdddddddddddmmm+                                                   
+echo                    +mms++mmdddmmydmdddddddmmmmmms                                                  
+echo                   sNNdooommdddmmhmmddddddmsoyNddN.                                                 
+echo                  .MmdNmmmmmmmdddddddddddmdoodNddNs                                                 
+echo                  .NddddmdmyhdNdddddddddddmdmmmmmNm                                                 
+echo                   mmdddddm++sMddddddddddddddmdosNy                                                 
+echo                   :mmddddmddmmddddddmdddddddmmhmo`                                                 
+echo                    .sddmmmddhyshoossyyyhhdmmmdo-                                                   
+echo                       .:+dyo+++ho+++ys++++yh-`                                                     
+echo                         -ys++++ohosh+++++++so-                                                     
+echo                       -yhs++++++ohho+++++++oos+.                   --                              
+echo                     :ysosh++++++++++++++++dyssh-                                                  
+echo                      -+o/-mo++++ssooss+++++d+:/:`                                                  
+echo                           ss++oh:    :y++++d/                                                      
+echo                           /h++hm      /y+++d.                                                      
+echo                           .d++hs       ds+od                                          `            
+echo                            +yyo`       .hys-       
+
+
+pause
+
+
+@echo off
+echo run the CMD (command prompt) program.
+pause
+start "" "https://www.youtube.com/watch?v=Q4_tIIHUIbQ"
+cls
+%systemRoot%\system32\cmd.exe
+
+
